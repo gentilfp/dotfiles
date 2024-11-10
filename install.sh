54e415d4361b0ef8b0b5c78872b8750704eb4cd5
Felipe Pelizaro <felipe.pelizaro@sequra.es>
2024-11-10 14:52:19 +0100

    Clean up

--- install.sh
+++
@@ -1,8 +0,0 @@
-echo 'Creating symbolic links...'
-
-ln -sf `realpath`/.tmux.conf ~/.tmux.conf
-ln -sf `realpath`/gentil.zsh-theme ~/.oh-my-zsh/themes/gentil.zsh-theme
-ln -sf `realpath`/init.vim ~/.config/nvim/init.vim
-
-echo 'Done'
-
