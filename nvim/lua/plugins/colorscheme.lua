154b2da7bc3f4e919fc1154880f06e94c6160f3b
Felipe Gentil <cdigentil@gmail.com>
2025-04-07 13:16:48 +0200

    Update

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,11 @@
 return {
   { "folke/tokyonight.nvim" },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "loctvl842/monokai-pro.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "monokai-pro",
     },
   },
 }
